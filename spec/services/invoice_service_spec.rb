d1bd5da8c37dd413009965b2bb5e9ed9fa41e0dd
erindrummond <e.b.drummond@gmail.com>
2013-04-30 18:49:01 -0600

    3 failing tests

--- spec/services/invoice_service_spec.rb
+++ spec/services/invoice_service_spec.rb
@@ -5,20 +5,22 @@ describe InvoiceService do
   it "Will create an invoice for each store" do
     store = stores(:test)
     order1 = Order.new(total_cost: 80, store_id: store.id)
-    order1.created_at = Date.new(2010,10,10)
+    order1.created_at = Date.today.ago(1.month)
     order1.save
     order2 = Order.new(total_cost: 120, store_id: store.id)
-    order2.created_at = Date.new(2010,10,11)
+    order2.created_at = Date.today.ago(1.month)
     order2.save
     orders = store.orders
-    InvoiceService.create(orders)
+    start_date = Date.today.ago(1.month).beginning_of_month
+    end_date = start_date.end_of_month
+    InvoiceService.create(orders, start_date, end_date)
     result = store.invoices.first
     expect(result).to_not be_nil
     expect(result.total_revenue).to eq 200
     expect(result.fee_amount).to eq 10
     expect(result.fee_percentage).to eq 5
-    expect(result.start_date).to eq Date.new(2010,10,10)
-    expect(result.end_date).to eq Date.new(2010,10,11)
+    expect(result.start_date).to eq Date.today.ago(1.month).beginning_of_month.to_date
+    expect(result.end_date).to eq Date.today.ago(1.month).end_of_month.to_date
     expect(result.status).to eq false
   end
 end
