d633a3b7bf0e402c3b270720bb78978b4387c8f1
novohispano@gmail.com <novohispano@gmail.com>
2013-04-30 20:03:41 -0600

    refund form working; ugly and probably needs original order displayed on the page with the form

--- app/controllers/admin/refunds_controller.rb
+++ app/controllers/admin/refunds_controller.rb
@@ -1,13 +1,15 @@
 class Admin::RefundsController < ApplicationController
   def new
-    @order = Order.find(params[:id])
+    @order      = Order.find(params[:id])
     @store_slug = params[:store_slug]
   end
 
   def create
-    @order = Order.find(params[:id])
-    RefundService.create(@order)
+    amount = params[:total_cost]
+    order  = Order.find(params[:id])
 
-    redirect_to admin_orders_path, :notice => "A refund for Order # #{@order.id} is been created"
+    RefundService.create(order, amount)
+
+    redirect_to admin_orders_path, :notice => "A refund for Order # #{order.id} is been created"
   end
 end
