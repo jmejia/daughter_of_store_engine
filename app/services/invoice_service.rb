668b4c05b4bd63fbd07e5d948c692d048333c2b8
Josh Mejia <joshua.mejia@gmail.com>
2013-04-30 22:26:26 -0600

    starting to adjust fees

--- app/services/invoice_service.rb
+++ app/services/invoice_service.rb
@@ -1,5 +1,5 @@
 class InvoiceService
-  def self.create(orders, start_date, end_date)
+  def self.create(orders, start_date, end_date, fee_percentage)
     order_fee = orders.inject(0){ |sum, order| sum + (order.total_cost || 0) }
     fee_amount = order_fee * 0.05
 
@@ -7,6 +7,7 @@ class InvoiceService
                     store_id:      orders.first.store_id,
                     start_date:    start_date,
                     end_date:      end_date,
-                    fee_amount:    fee_amount)
+                    fee_amount:    fee_amount,
+                    fee_percentage: fee_percentage)
   end
 end
