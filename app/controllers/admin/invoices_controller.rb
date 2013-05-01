72416a3936d3f7e0e4e971fa7ece543d7f3cc388
Josh Mejia <joshua.mejia@gmail.com>
2013-05-01 10:37:11 -0600

    adjusted the outstanding_balance to use start_date instead of created_at

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -6,7 +6,7 @@ class Admin::InvoicesController < ApplicationController
     @url_date = Date.new(params[:year].to_i, params[:month].to_i)
     @previous_month_start_date = @url_date - 1.month
     @total_fees = Order.total_monthly_fees(@previous_month_start_date)
-    @outstanding_balance = Invoice.outstanding_balance(@url_date)
+    @outstanding_balance = Invoice.outstanding_balance(@previous_month_start_date)
     @monthly_invoices = Invoice.monthly_invoices?(@previous_month_start_date)
     @stores = Store.all
     @existing_invoices = Invoice.monthly_invoices?(@previous_month_start_date)
@@ -35,7 +35,7 @@ class Admin::InvoicesController < ApplicationController
     invoice = Invoice.find(params[:id])
     if invoice.update_attributes(params[:invoice])
       flash.now[:alert] = "Successfully changed status of invoice."
-      @url_date = Date.new(params[:year].to_i, params[:month].to_i)
+      @url_date = Date.new(params[:year].to_i, params[:month].to_i) - 1.month
       @outstanding_balance = Invoice.outstanding_balance(@url_date)
     else
       flash.now[:error] = "Unable to mark invoice as paid"
